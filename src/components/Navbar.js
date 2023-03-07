9986b377b1dd91556df0a8785588d973ac553d37
wellington <wrogeriowrs@gmail.com>
2023-03-07 19:15:57 -0300

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
                 <Link className="navbar-brand" href="/">
-                    WR Service Desk
+                    Service Desk
                 </Link>
                 <button onClick={() => toggleMenu()} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
