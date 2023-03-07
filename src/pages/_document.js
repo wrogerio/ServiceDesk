5ec3d5b3b5909cea8a994ca5026ba9dcbe16fbaf
wellington <wrogeriowrs@gmail.com>
2023-03-07 19:15:15 -0300

    .

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://cdn.staticaly.com/gh/hung1001/font-awesome-pro/4cac1a6/css/all.css" rel="stylesheet" type="text/css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
