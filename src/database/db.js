bda19c6ccf5516cab97a14c6115c1b136314f313
wellington <wrogeriowrs@gmail.com>
2023-03-08 16:37:00 -0300

    .

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,10 @@
 const sql = require("mssql");
+console.log(process.env)
 const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     server: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: parseInt(process.env.DB_PORT),
     database: process.env.DB_NAME,
     options: {
         encrypt: true,
