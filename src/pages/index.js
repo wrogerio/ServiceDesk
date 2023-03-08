4f5a193827f895caa7c37e8b989cadeb235a3b33
wellington <wrogeriowrs@gmail.com>
2023-03-08 20:06:35 -0300

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Navbar from './../components/Navbar';
+import { use, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/chamados')
+  }, [])
+
   return (
     <>
       <main>
